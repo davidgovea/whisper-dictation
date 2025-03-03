f25463c7d5fb06fc143edcda7e1cebfef17e3748
David Govea <david.govea@lattice.com>
2025-03-03 11:32:04 -0500

    small.en-mlx-8bit works nice for me

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 cd "$(dirname "$0")"
 while true; do
   echo "Starting whisper-dictation..."
-  python whisper-dictation.py -m small --ptt
+  python whisper-dictation.py -m mlx-community/whisper-small.en-mlx-8bit --ptt
   echo "Finished"
 done
